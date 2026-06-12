2e85fed9eca3dd8de28810953c576dc1882ce0a8
Rockhandz <rockhands@hotmail.com>
2026-06-12 23:12:16 +0100

    x

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,10 +1,46 @@
-console.log("Hello World");
+
+// Create function to get computer choice
+ 
+function getComputerChoice() {
+
+    let choice = Math.random()
+    console.log(choice)
+
+    if (choice <= 0.33333) console.log("Rock"); 
+    else if (choice >= 0.66666) console.log("Scissors");
+    else console.log("Paper");
+
+}
+
+getComputerChoice()
 
 
-function getCompterChoice(Rock,Paper,Scissors) {
+//create function to get user choice
+
+
+function getHumanChoice (){
+
+    let human_choice = prompt("Rock, Paper, Scissors ?")
+    if (human_choice == "Rock") console.log("Rock");
+    else if (human_choice == "Paper") console.log("Paper");
+    else if (human_choice == "Scissors") console.log("Scissors");
+    else console.log ("Error")
+ }
+
+getHumanChoice()
+
+
+var humanScore = 0
+
+var computerScore = 0
+
+
+function playRound (humanChoice, computerChoice) {
 
 
 }
 
-let Scissors = Math.random() * 100 + 1;
-    console.log(Scissors);
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
